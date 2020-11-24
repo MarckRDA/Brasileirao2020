58a0bd684e3558080ad5dceb3fdba04a0c92b7d7
MarckRDA <marcos.edo132@gmail.com>
2020-11-23 21:25:01 -0300

    Added Jogadores, Times, Tabela de Estatiticas table in migrations

---
+++ Domain/Migrations/20201124001919_JogadoresTimeTabelaCreate.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class JogadoresTimeTabelaCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TabelasEstatistica",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TimeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Pontuacao = table.Column<int>(type: "int", nullable: false),
+                    PartidasDisputadas = table.Column<int>(type: "int", nullable: false),
+                    Vitorias = table.Column<int>(type: "int", nullable: false),
+                    Derrotas = table.Column<int>(type: "int", nullable: false),
+                    Empate = table.Column<int>(type: "int", nullable: false),
+                    GolsPro = table.Column<int>(type: "int", nullable: false),
+                    GolsContra = table.Column<int>(type: "int", nullable: false),
+                    SaldoDeGols = table.Column<int>(type: "int", nullable: false),
+                    PercentagemAproveitamento = table.Column<double>(type: "float", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabelasEstatistica", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Times",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NomeTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TabelaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Times", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Times_TabelasEstatistica_TabelaId",
+                        column: x => x.TabelaId,
+                        principalTable: "TabelasEstatistica",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jogadores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Gol = table.Column<int>(type: "int", nullable: false),
+                    Fk_Time = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TimeId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Times_Fk_Time",
+                        column: x => x.Fk_Time,
+                        principalTable: "Times",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Times_TimeId1",
+                        column: x => x.TimeId1,
+                        principalTable: "Times",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_Fk_Time",
+                table: "Jogadores",
+                column: "Fk_Time");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_TimeId1",
+                table: "Jogadores",
+                column: "TimeId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Times_TabelaId",
+                table: "Times",
+                column: "TabelaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogadores");
+
+            migrationBuilder.DropTable(
+                name: "Times");
+
+            migrationBuilder.DropTable(
+                name: "TabelasEstatistica");
+        }
+    }
+}
